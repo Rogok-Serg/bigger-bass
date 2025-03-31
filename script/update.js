949c326fe63e1151c38960138fb7f8844432d6e2
Rogok-Serg <rogoksv@gmail.com>
2025-03-31 13:01:11 +0300

    update window

---
+++ script/update.js
@@ -0,0 +1,17 @@
+let touchStartY = 0;
+let touchEndY = 0;
+
+window.addEventListener("touchstart", (e) => {
+  touchStartY = e.changedTouches[0].screenY;
+});
+
+window.addEventListener("touchmove", (e) => {
+  touchEndY = e.changedTouches[0].screenY;
+});
+
+window.addEventListener("touchend", () => {
+  if (touchEndY > touchStartY + 50) {
+    // перевірка, чи свайп був достатньо довгим
+    location.reload(); // оновлення сторінки
+  }
+});
